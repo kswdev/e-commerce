ca6742c73e93ff8ab47db4a2599e0223cc7b366d
ksw <seonwongim@gmail.com>
2026-06-19 16:24:35 +0900

    Product-Vendor 1:N 엔티티그래프 적용

--- e-commerce-admin-monolithic/src/main/java/net/study/ecommerceadminmonolithic/repository/product/ProductRepository.java
+++ e-commerce-admin-monolithic/src/main/java/net/study/ecommerceadminmonolithic/repository/product/ProductRepository.java
@@ -3,11 +3,13 @@ package net.study.ecommerceadminmonolithic.repository.product;
 import net.study.ecommerceadminmonolithic.repository.product.entity.ProductEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
+    @EntityGraph(attributePaths = "vendor")
     Page<ProductEntity> findAll(Pageable pageable);
 }
