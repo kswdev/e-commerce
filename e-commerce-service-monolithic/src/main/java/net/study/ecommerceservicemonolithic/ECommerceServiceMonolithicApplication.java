7a4d79ca631a44a809daebebced480cf2647795f
ksw <seonwongim@gmail.com>
2026-07-08 16:00:26 +0900

    이커머스 서비스 초기화

---
+++ e-commerce-service-monolithic/src/main/java/net/study/ecommerceservicemonolithic/ECommerceServiceMonolithicApplication.java
@@ -0,0 +1,13 @@
+package net.study.ecommerceservicemonolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceServiceMonolithicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceServiceMonolithicApplication.class, args);
+    }
+
+}
---
+++ e-commerce-service-monolithic/src/test/java/net/study/ecommerceservicemonolithic/ECommerceServiceMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.study.ecommerceservicemonolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceServiceMonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
