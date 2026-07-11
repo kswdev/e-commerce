d1bf012d04a9651c8fd7d2c279142a02bcc0c4a8
ksw <seonwongim@gmail.com>
2026-07-11 20:24:34 +0900

    대쉬보드 페이지 동적으로 지표를 수집하기 위해 값 상태로 추출

--- sb-admin-2-react/src/pages/Dashboard/index.tsx
+++ sb-admin-2-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import Sidebar from '../../components/Navigation/Sidebar';
 import Topbar from '../../components/Navigation/Topbar';
 import CardInfo from '../../components/Cards/Info';
@@ -6,52 +6,67 @@ import ChartDonut from '../../components/Charts/Donut';
 import ChartLine from '../../components/Charts/Line';
 import PageHeading from '../../components/PageHeading';
 
-class Dashboard extends Component {
-  UNSAFE_componentWillMount() {
-    document.getElementById('body')!.className = 'page-top';
-  }
+const Dashboard = () => {
 
-  render() {
+    const [dailyCustomerJoinCnt, setDailyCustomerJoinCnt] = useState<number>(0);
+    const [dailyCustomerQuitCnt, setDailyCustomerQuitCnt] = useState<number>(0);
+    const [dailyPaymentCnt, setDailyPaymentCnt] = useState<number>(0);
+    const [dailyCancelCnt, setDailyCancelCnt] = useState<number>(0);
+    
+    useEffect(() => {
+        document.getElementById('body')!.className = 'page-top';
+    });
+    
+    
     return (
-      <div>
-        <div id="wrapper">
-          <Sidebar />
-          <div id="content-wrapper" className="d-flex flex-column">
-            <div id="content">
-              <Topbar />
-              <div className="container-fluid">
-                <PageHeading title="Dashboard" />
-                <div className="row">
-                  <CardInfo title="Earnings (Monthly)" icon="calendar" color="primary" value="$40,000" />
-                  <CardInfo title="Earnings (Annual)"  icon="calendar" color="success" value="215,000" />
-                  <CardInfo title="Tasks"              icon="clipboard" color="info"    value="50%"     />
-                  <CardInfo title="Pending Requests"   icon="comments"  color="warning" value="18"      />
+        <div>
+            <div id="wrapper">
+                <Sidebar/>
+                <div id="content-wrapper" className="d-flex flex-column">
+                    <div id="content">
+                        <Topbar/>
+                        <div className="container-fluid">
+                            <PageHeading title="Dashboard"/>
+                            <div className="row">
+                                {/* 일별 결제 갯수 */}
+                                <CardInfo title="payments (daily)" icon="calendar" color="primary"
+                                          value={dailyPaymentCnt}/>
+                                {/* 일별 취소 갯수 */}
+                                <CardInfo title="cancels (daily)" icon="calendar" color="warning"
+                                          value={dailyCancelCnt}/>
+
+                                {/* 일별 가입 고객수 */}
+                                <CardInfo title="Joins (daily)" icon="clipboard" color="info"
+                                          value={dailyCustomerJoinCnt}/>
+
+                                {/* 일별 탈퇴 갯수 */}
+                                <CardInfo title="Quits (daily)" icon="comments" color="warning"
+                                          value={dailyCustomerQuitCnt}/>
+                            </div>
+                            <div className="row">
+                                <div className="col-xl-8 col-lg-6">
+                                    <ChartLine/>
+                                </div>
+                                <div className="col-xl-4 col-lg-6">
+                                    <ChartDonut/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <footer className="sticky-footer bg-white">
+                        <div className="container my-auto">
+                            <div className="copyright text-center my-auto">
+                                <span>Copyright &copy; Your Website 2019</span>
+                            </div>
+                        </div>
+                    </footer>
                 </div>
-                <div className="row">
-                  <div className="col-xl-8 col-lg-6">
-                    <ChartLine />
-                  </div>
-                  <div className="col-xl-4 col-lg-6">
-                    <ChartDonut />
-                  </div>
-                </div>
-              </div>
             </div>
-            <footer className="sticky-footer bg-white">
-              <div className="container my-auto">
-                <div className="copyright text-center my-auto">
-                  <span>Copyright &copy; Your Website 2019</span>
-                </div>
-              </div>
-            </footer>
-          </div>
+            <a className="scroll-to-top rounded" href="#page-top">
+                <i className="fas fa-angle-up"></i>
+            </a>
         </div>
-        <a className="scroll-to-top rounded" href="#page-top">
-          <i className="fas fa-angle-up"></i>
-        </a>
-      </div>
     );
-  }
 }
 
 export default Dashboard;
