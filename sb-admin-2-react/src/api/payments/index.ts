b65f5916651809c49ad5152df653da9466b19666
ksw <seonwongim@gmail.com>
2026-07-20 15:02:16 +0900

    결제 조회 url 수정

--- sb-admin-2-react/src/api/payments/index.ts
+++ sb-admin-2-react/src/api/payments/index.ts
@@ -2,10 +2,10 @@ import axiosInstance from '../axiosInstance';
 
 export const getDailyPaymentCnt = () =>
     axiosInstance
-        .get<number>('/payments/cnt')
+        .get<number>('/payments/daily-cnt')
         .then((res) => res.data);
 
 export const getDailyCancelCnt = () =>
     axiosInstance
-        .get<number>('/payments/cancel-cnt')
+        .get<number>('/payments/daily-cancel-cnt')
         .then((res) => res.data);
