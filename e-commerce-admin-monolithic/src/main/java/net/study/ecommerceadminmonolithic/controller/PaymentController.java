801652cae6d4e864d686783a56a523e49b65d9ce
ksw <seonwongim@gmail.com>
2026-07-20 20:39:41 +0900

    PaymentController - 기능 미비 로그 추가

--- e-commerce-admin-monolithic/src/main/java/net/study/ecommerceadminmonolithic/controller/PaymentController.java
+++ e-commerce-admin-monolithic/src/main/java/net/study/ecommerceadminmonolithic/controller/PaymentController.java
@@ -1,9 +1,11 @@
 package net.study.ecommerceadminmonolithic.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/payments")
 public class PaymentController {
@@ -11,12 +13,16 @@ public class PaymentController {
     @GetMapping("daily-cnt")
     public int getPaymentCount() {
         // TODO: 일간 결제 서비스 기능 추가
+
+        log.info("PaymentController.getPaymentCount() - 더미 데이터");
         return 3;
     }
 
     @GetMapping("daily-cancel-cnt")
     public int getPaymentCancelCount() {
         // TODO: 일간 결제 취소 서비스 기능 추가
+
+        log.info("PaymentController.getPaymentCancelCount() - 더미 데이터");
         return 2;
     }
 }
